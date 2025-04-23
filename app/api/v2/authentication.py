8b00437c246cabb638c60fc03f3fa1f7cc2a6aa7
7mochi <flyingcatdm@gmail.com>
2025-04-22 22:09:05 -0500

    feat: add /register endpoint; add hcaptcha to registration and authentication endpoints

--- app/api/v2/authentication.py
+++ app/api/v2/authentication.py
@@ -5,15 +5,21 @@ from collections.abc import Awaitable
 from collections.abc import Callable
 from typing import Any
 
+import bcrypt
 from fastapi import APIRouter
 from fastapi import Cookie
+from fastapi import Request
 from fastapi import Response
 from fastapi import status
 from fastapi.responses import JSONResponse
 
+import app.settings
 from app.api.v2.common import responses
 from app.api.v2.common.responses import Failure
 from app.api.v2.models.authentication import AuthenticationRequest
+from app.api.v2.models.authentication import RegistrationRequest
+from app.constants import regexes
+from app.repositories import stats as stats_repo
 from app.repositories import tokens
 from app.repositories import tokens as tokens_repo
 from app.repositories import users as users_repo
@@ -70,6 +76,21 @@ def authenticate_user_session(
 
 @router.post("/authenticate")
 async def authenticate(args: AuthenticationRequest) -> Any:
+    response = await app.state.services.http_client.post(
+        "https://hcaptcha.com/siteverify",
+        data={
+            "secret": app.settings.HCAPTCHA_SECRET_KEY,
+            "response": args.hcaptcha_token,
+        },
+    )
+    response_data = response.json()
+
+    if not response_data["success"]:
+        return responses.failure(
+            message="Invalid captcha.",
+            status_code=status.HTTP_400_BAD_REQUEST,
+        )
+
     _response = await authentication.authenticate(
         username=args.username,
         password=args.password,
@@ -143,3 +164,136 @@ async def authorize_request(
         return None
 
     return trusted_access_token
+
+
+@router.post("/register")
+async def register(request: Request, args: RegistrationRequest) -> Any:
+    response = await app.state.services.http_client.post(
+        "https://hcaptcha.com/siteverify",
+        data={
+            "secret": app.settings.HCAPTCHA_SECRET_KEY,
+            "response": args.hcaptcha_token,
+        },
+    )
+    response_data = response.json()
+
+    if not response_data["success"]:
+        return responses.failure(
+            message="Invalid captcha.",
+            status_code=status.HTTP_400_BAD_REQUEST,
+        )
+
+    # Usernames must:
+    # - be within 2-15 characters in length
+    # - not contain both ' ' and '_', one is fine
+    # - not be in the config's `disallowed_names` list
+    # - not already be taken by another player
+    if not regexes.USERNAME.match(args.username):
+        return responses.failure(
+            message="Username must be between 2 and 15 characters long.",
+            status_code=status.HTTP_400_BAD_REQUEST,
+        )
+
+    if "_" in args.username and " " in args.username:
+        return responses.failure(
+            message='Username may contain "_" and " ", but not both.',
+            status_code=status.HTTP_400_BAD_REQUEST,
+        )
+
+    if args.username in app.settings.DISALLOWED_NAMES:
+        return responses.failure(
+            message="Disallowed username; pick another.",
+            status_code=status.HTTP_400_BAD_REQUEST,
+        )
+
+    if await users_repo.fetch_one(name=args.username):
+        return responses.failure(
+            message="Username already taken by another player.",
+            status_code=status.HTTP_400_BAD_REQUEST,
+        )
+
+    # Emails must:
+    # - match the regex `^[^@\s]{1,200}@[^@\s\.]{1,30}\.[^@\.\s]{1,24}$`
+    # - not already be taken by another player
+    if not regexes.EMAIL.match(args.email):
+        return responses.failure(
+            message="Invalid email syntax.",
+            status_code=status.HTTP_400_BAD_REQUEST,
+        )
+    else:
+        if await users_repo.fetch_one(email=args.email):
+            return responses.failure(
+                message="Email already taken by another player.",
+                status_code=status.HTTP_400_BAD_REQUEST,
+            )
+
+    # Passwords must:
+    # - be within 8-32 characters in length
+    # - have more than 3 unique characters
+    # - not be in the config's `disallowed_passwords` list
+    if not 8 <= len(args.password) <= 32:
+        return responses.failure(
+            message="Password must be between 8 and 32 characters long.",
+            status_code=status.HTTP_400_BAD_REQUEST,
+        )
+
+    if len(set(args.password)) <= 3:
+        return responses.failure(
+            message="Password must contain more than 3 unique characters.",
+            status_code=status.HTTP_400_BAD_REQUEST,
+        )
+
+    if args.password.lower() in app.settings.DISALLOWED_PASSWORDS:
+        return responses.failure(
+            message="That password was deemed too simple.",
+            status_code=status.HTTP_400_BAD_REQUEST,
+        )
+
+    ip = app.state.services.ip_resolver.get_ip(request.headers)
+    geoloc = await app.state.services.fetch_geoloc(ip, request.headers)
+
+    pw_md5 = hashlib.md5(args.password.encode()).hexdigest().encode()
+    pw_bcrypt = bcrypt.hashpw(pw_md5, bcrypt.gensalt())
+
+    country = geoloc["country"]["acronym"] if geoloc is not None else "xx"
+
+    user = await users_repo.create(
+        name=args.username,
+        email=args.email,
+        pw_bcrypt=pw_bcrypt,
+        country=country,
+    )
+
+    if country != "PE":
+        return responses.failure(
+            message="Registration is only available to Peruvian players.",
+            status_code=status.HTTP_400_BAD_REQUEST,
+        )
+
+    await stats_repo.create_all_modes(player_id=user["id"])
+
+    auth_response = await authentication.authenticate(
+        username=args.username,
+        password=args.password,
+    )
+
+    if auth_response is None:
+        return responses.failure(
+            message="Failed to authenticate after registration.",
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
+
+    http_response = JSONResponse(
+        content=auth_response.identity.model_dump(),
+        status_code=status.HTTP_201_CREATED,
+    )
+    http_response.set_cookie(
+        "X-Bpy-Token",
+        value=auth_response.unhashed_access_token,
+        expires=60 * 60 * 24 * 30,
+        secure=True,
+        httponly=True,
+        samesite="none",
+    )
+
+    return http_response
